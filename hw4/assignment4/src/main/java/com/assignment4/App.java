13c8740846a498dd362ff999c8f9b9fbca88af45
CieranW <cieranwong@gmail.com>
2025-02-13 20:19:24 -0700

    Data and loadData func

--- hw4/assignment4/src/main/java/com/assignment4/App.java
+++ hw4/assignment4/src/main/java/com/assignment4/App.java
@@ -1,5 +1,9 @@
 package com.assignment4;
 
+import java.io.BufferedReader;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,32 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public final void loadData() {
+        URL url = null;
+        BufferedReader in = null;
+
+        try {
+            url = new URL("https://sample-videos.com/csv/Sample-Spreadsheet-1000-rows.csv");
+            in = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.ISO_8859_1));
+            String inputLine;
+            
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+        } catch (MalformedURLException e2) {
+            e2.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+            try {
+                if (in != null ) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
