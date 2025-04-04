4f303a5a43b21368f409e3367b32a0be391a11e3
CieranW <cieranwong@gmail.com>
2025-04-03 19:59:35 -0700

    100 ms delay and 1000 products

--- hw9/tmp/src/main/java/thread/Consumer.java
+++ hw9/tmp/src/main/java/thread/Consumer.java
@@ -11,9 +11,13 @@ class Consumer extends Thread {
 
 	public void run() {
 		int value = 0;
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 1000; i++) {
 			value = cubbyhole.get();
 			System.out.println("Consumer #" + this.number + " got: " + value + " :: "+System.currentTimeMillis());
+			try {
+				sleep(100);
+			} catch (InterruptedException e) {
+			}
 		}
 	}
 }
